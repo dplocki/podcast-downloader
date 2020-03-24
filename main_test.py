1e8899122ef99e64f8f658c030f8ab1b4d87c7ca
Dawid Płocki <dawid.plocki@gmail.com>
2020-03-24 21:27:06 +0100

    Improving unit tests for only_new_entites method

--- main_test.py
+++ main_test.py
@@ -25,19 +25,27 @@ class TestLink2FileName(unittest.TestCase):
 
 class TestFindNewRSSEntites(unittest.TestCase):
 
-    def test_find_one_rss_entites(self):
+    def test_find_all_new_rss_entites(self):
         # Assing
-        new_one = RSSEntity((2020, 1, 2, 17, 3, 38, 1, 48, 0), 'http://www.p.com/file0002.mp3')
-        old_one = RSSEntity((2020, 1, 1, 11, 3, 38, 1, 48, 0), 'http://www.p.com/file0001.mp3')
-        rss_entites = [new_one, old_one]
-        downloaded = ['[20200101] file0001.mp3']
-        expected = [new_one]
+        new_one_1 = RSSEntity((2020, 1, 10, 17, 3, 38, 1, 48, 0), 'http://www.p.com/file0005.mp3')
+        new_one_2 = RSSEntity((2020, 1, 9, 17, 3, 38, 1, 48, 0), 'http://www.p.com/file0004.mp3')
+        old_one_1 = RSSEntity((2020, 1, 8, 11, 3, 38, 1, 48, 0), 'http://www.p.com/file0003.mp3')
+        old_one_2 = RSSEntity((2020, 1, 7, 11, 3, 38, 1, 48, 0), 'http://www.p.com/file0002.mp3')
+        old_one_3 = RSSEntity((2020, 1, 6, 11, 3, 38, 1, 48, 0), 'http://www.p.com/file0001.mp3')
+
+        rss_entites = [new_one_1, new_one_2, old_one_1, old_one_2, old_one_3]
+        downloaded = [
+            '[20200108] file0003.mp3',
+            '[20200107] file0002.mp3',
+            '[20200106] file0001.mp3']
+
+        expected = [new_one_1, new_one_2]
 
         # Act
         result = list(only_new_entites(rss_entites, downloaded))
 
         # Assert
-        self.assertSequenceEqual(result, expected, "A new_one entity should be return as result")
+        self.assertSequenceEqual(result, expected, "A both new entity should be return as result")
 
 
 if __name__ == '__main__':
