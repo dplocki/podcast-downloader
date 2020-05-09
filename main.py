c6cf86b3edb8483f862a7dd024c4c70f5dc1540f
Dawid Płocki <dawid.plocki@gmail.com>
2020-05-09 17:59:00 +0200

    Splitting the large function get_downloaded_files into smaller ones

--- main.py
+++ main.py
@@ -27,15 +27,24 @@ def load_configuration_file(file_path):
 
 # Downloaded directory
 
+def only_mp3(file_name: str) -> bool:
+    return file_name.endswith('.mp3')
+
+def is_file(directory_path: str, file_name: str) -> bool:
+    return os.path.isfile(os.path.join(directory_path, file_name))
+
+def get_files_from(directory_path: str):
+    return sorted(os.listdir(directory_path), reverse=True)
+
+def get_downloaded_files(podcast_directory: str) -> [str]:
+    is_directory_file = partial(is_file, podcast_directory)
+
+    return (file
+            for file in get_files_from(podcast_directory)
+            if only_mp3(file) and is_directory_file(file))
+
 def get_last_downloaded(podcast_directory: str):
-
-    def get_downloaded_files(podcast_directory: str) -> [str]:
-        return (file
-                for file in sorted(os.listdir(podcast_directory), reverse=True)
-                if file.endswith('.mp3') and os.path.isfile(os.path.join(podcast_directory, file)))
-
     return next(get_downloaded_files(podcast_directory))
-
 
 # RSS
 
