c0cff103d260989d569cf8334ced4eb1402db1b9
Dawid Płocki <dawid.plocki@gmail.com>
2020-05-19 20:16:43 +0200

    Improve the setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,13 @@ setup(
     long_description_content_type='text/markdown',
     long_description=read('README.md'),
     packages=['podcast_downloader'],
+    install_requires=['feedparser'],
     classifiers=[
         'Environment :: Console',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.7'
-    ]
+    ],
+    python_requires='>=3.6'
 )
