f5f56e932c0ab321cd1b6ff2c9c267f360552045
Dawid Płocki <dawid.plocki@gmail.com>
2020-06-10 20:04:17 +0200

    Create entities generator

--- tests/commons.py
+++ tests/commons.py
@@ -1,2 +1,16 @@
+from podcast_downloader.rss import RSSEntityWithDate
+
+
 def build_timestamp(year: int, month: int, day: int):
     return (year, month, day, 17, 3, 38, 1, 48, 0)
+
+
+def rss_entity_generator(day: int = 10, file_number: int = 5, limit: int = 5) -> list:
+    while limit:
+        yield RSSEntityWithDate(
+            build_timestamp(2020, 1, day),
+            f'http://www.p.com/file{file_number:0>4}.mp3')
+
+        day -= 1
+        file_number -= 1
+        limit -= 1
--- tests/find_new_rss_entities_test.py
+++ tests/find_new_rss_entities_test.py
@@ -1,22 +1,14 @@
 import unittest
 
-from podcast_downloader.rss import\
-    RSSEntityWithDate,\
-    only_new_entites
-
-from commons import build_timestamp
+from podcast_downloader.rss import only_new_entites
+from commons import rss_entity_generator
 
 
 class TestFindNewRSSEntites(unittest.TestCase):
 
     def test_find_all_new_rss_entites(self):
         # Assign
-        new_one_1 = RSSEntityWithDate(build_timestamp(2020, 1, 10), 'http://www.p.com/file0005.mp3')
-        new_one_2 = RSSEntityWithDate(build_timestamp(2020, 1, 9), 'http://www.p.com/file0004.mp3')
-        old_one_1 = RSSEntityWithDate(build_timestamp(2020, 1, 8), 'http://www.p.com/file0003.mp3')
-        old_one_2 = RSSEntityWithDate(build_timestamp(2020, 1, 7), 'http://www.p.com/file0002.mp3')
-        old_one_3 = RSSEntityWithDate(build_timestamp(2020, 1, 6), 'http://www.p.com/file0001.mp3')
-
+        new_one_1, new_one_2, old_one_1, old_one_2, old_one_3 = rss_entity_generator(limit=5)
         rss_entites = [new_one_1, new_one_2, old_one_1, old_one_2, old_one_3]
         last_downloaded_file = '[20200108] file0003.mp3'
 
