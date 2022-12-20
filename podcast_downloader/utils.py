15b2202d30cb99a105ff9e85970235cf36314263
Dawid Płocki <dawid.plocki@gmail.com>
2022-12-20 18:00:02 +0100

    Reduce usage of RSSEntity constructor in tests

--- podcast_downloader/utils.py
+++ podcast_downloader/utils.py
@@ -3,15 +3,23 @@ from datetime import datetime
 
 
 def mark_parameters_in_message(message: str, *parameters) -> str:
-    return message.replace("{}", "\033[97m{}\033[0m").format(*parameters) if parameters else message
+    return (
+        message.replace("{}", "\033[97m{}\033[0m").format(*parameters)
+        if parameters
+        else message
+    )
 
 
 def log(message, *parameters):
-    print(f"[\033[2m{datetime.now():%Y-%m-%d %H:%M:%S}\033[0m] {mark_parameters_in_message(message, *parameters)}")
+    print(
+        f"[\033[2m{datetime.now():%Y-%m-%d %H:%M:%S}\033[0m] {mark_parameters_in_message(message, *parameters)}"
+    )
 
 
 def warning(message, *parameters):
-    print(f"[\033[2m{datetime.now():%Y-%m-%d %H:%M:%S}\033[0m] \033[33mWarning:\033[0m {mark_parameters_in_message(message, *parameters)}")
+    print(
+        f"[\033[2m{datetime.now():%Y-%m-%d %H:%M:%S}\033[0m] \033[33mWarning:\033[0m {mark_parameters_in_message(message, *parameters)}"
+    )
 
 
 def compose(*functions):
--- tests/link_to_filename_test.py
+++ tests/link_to_filename_test.py
@@ -1,17 +1,21 @@
+from datetime import datetime
+from typing import Tuple
 import unittest
 
 from podcast_downloader.rss import RSSEntity, to_name_with_date_name, to_plain_file_name
 from tests.commons import build_timestamp
 
 
+def build_test_link_data(link: str) -> Tuple[datetime.date, str, str]:
+    return RSSEntity(build_timestamp(2020, 1, 2), "audio/mp3", "The fancy title", link)
+
+
 class TestLink2FileName(unittest.TestCase):
     def test_entry_to_simple_file_name(self):
         # Assign
         expected = "file_name.mp3"
-        rss_entry = RSSEntity(
-            build_timestamp(2020, 1, 2),
-            "audio/mp3",
-            "http://www.podcast.com/podcast/something/fIlE_nAme.mp3",
+        rss_entry = build_test_link_data(
+            link="http://www.podcast.com/podcast/something/fIlE_nAme.mp3"
         )
 
         # Act
@@ -25,10 +29,8 @@ class TestLink2FileName(unittest.TestCase):
     def test_entry_to_file_name_with_date(self):
         # Assign
         expected = "[20200102] file_name.mp3"
-        rss_entry = RSSEntity(
-            build_timestamp(2020, 1, 2),
-            "audio/mp3",
-            "http://www.podcast.com/podcast/something/fIlE_nAme.mp3",
+        rss_entry = build_test_link_data(
+            link="http://www.podcast.com/podcast/something/fIlE_nAme.mp3"
         )
 
         # Act
