6686624b14d67bd6b5458d37e20910e16d3c1061
Dawid Płocki <dawid.plocki@gmail.com>
2024-01-10 20:21:02 +0100

    Implement the config builder

--- e2e/test_sample.py
+++ e2e/test_sample.py
@@ -1,14 +1,18 @@
 import os
 import subprocess
 import sys
+import tempfile
 import pytest
+import json
 
 
 @pytest.fixture()
 def secure_config_file():
     home_directory = os.path.expanduser("~")
     config_file_name = os.path.join(home_directory, ".podcast_downloader_config.json")
-    backup_config_file_name = os.path.join(home_directory, ".safe_copy_podcast_downloader_config.json")
+    backup_config_file_name = os.path.join(
+        home_directory, ".safe_copy_podcast_downloader_config.json"
+    )
 
     if os.path.exists(config_file_name):
         os.rename(config_file_name, backup_config_file_name)
@@ -20,17 +24,23 @@ def secure_config_file():
         os.rename(backup_config_file_name, config_file_name)
 
 
+@pytest.fixture()
+def create_temporary_directory():
+    with tempfile.TemporaryDirectory() as tmp_dirname:
+        yield tmp_dirname
+
+
 def build_server():
     pass
 
 
-def build_config(config_path):
-    with open(config_path, 'w') as file:
-        file.write('''{
-        }''')
+def build_config(config_path, config_object):
+    with open(config_path, "w") as file:
+        file.write(json.dumps(config_object))
 
 
 def run_podcast_downloader():
+    os.system("cat ~/.podcast_downloader_config.json")
     subprocess.check_call([sys.executable, "-m", "podcast_downloader"])
 
 
@@ -38,9 +48,20 @@ def check_the_download_directory():
     pass
 
 
-def test_answer(secure_config_file):
+def test_answer(secure_config_file, create_temporary_directory):
     build_server()
-    build_config(secure_config_file)
+    build_config(
+        secure_config_file,
+        {
+            "podcasts": [
+                {
+                    "name": "test",
+                    "path": create_temporary_directory,
+                    "rss_link": "feed",
+                }
+            ]
+        },
+    )
 
     run_podcast_downloader()
 
