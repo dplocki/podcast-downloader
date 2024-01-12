3056351f11585fb0505ce8c22915d198291a53db
Dawid Płocki <dawid.plocki@gmail.com>
2024-01-12 20:51:50 +0100

    Introduce files to download

--- e2e/test_sample.py
+++ e2e/test_sample.py
@@ -32,32 +32,60 @@ def create_temporary_directory():
         yield tmp_dirname
 
 
-class FeedBuilder():
+@pytest.fixture()
+def create_origin_feed_directory(create_temporary_directory):
+    feed_source_path = os.path.join(create_temporary_directory, "feed_source")
+    os.makedirs(feed_source_path)
+    yield feed_source_path
 
-    def __init__(self) -> None:
+
+@pytest.fixture()
+def create_download_destination_directory(create_temporary_directory):
+    feed_destination_path = os.path.join(create_temporary_directory, "destination")
+    os.makedirs(feed_destination_path)
+    yield feed_destination_path
+
+
+class FeedBuilder:
+    def __init__(self, feed_source) -> None:
+        self.feed_source = feed_source
         self.fg = FeedGenerator()
-        self.fg.title('Some Testfeed')
-        self.fg.author( {'name':'John Doe','email':'john@example.de'} )
-        self.fg.subtitle('This is a cool feed!')
-        self.fg.link( href='http://example.com', rel='alternate' )
-
+        self.fg.title("Some Testfeed")
+        self.fg.author({"name": "John Doe", "email": "john@example.de"})
+        self.fg.subtitle("This is a cool feed!")
+        self.fg.link(href="http://example.com", rel="alternate")
 
     def add_entry(self):
         fe = self.fg.add_entry()
-        fe.id('http://lernfunk.de/media/654321/1/file.mp3')
-        fe.title('The First Episode')
-        fe.description('Enjoy our first episode.')
-        fe.enclosure('http://lernfunk.de/media/654321/1/file.mp3', 0, 'audio/mpeg')
-        fe.published(datetime.datetime(2014, 7, 10, 2, 43, 55, 230107, tzinfo=datetime.timezone.utc))
+        fe.id("http://lernfunk.de/media/654321/1/file.mp3")
+        fe.title("The First Episode")
+        fe.description("Enjoy our first episode.")
+        fe.enclosure(f"file:///{self.feed_source}/file.mp3", 0, "audio/mpeg")
+        fe.published(
+            datetime.datetime(
+                2014, 7, 10, 2, 43, 55, 230107, tzinfo=datetime.timezone.utc
+            )
+        )
+
+        self.__add_file_in_source("file.mp3")
 
         return self
 
-
     def build(self):
-        rss_file_name = 'podcast.xml'
+        rss_file_name = "podcast.xml"
         self.fg.rss_file(rss_file_name)
 
         return rss_file_name
+
+    def __add_file_in_source(self, file_name):
+        path_to_file = os.path.join(self.feed_source, file_name)
+        with open(path_to_file, "w") as file:
+            file.write("test")
+
+
+@pytest.fixture()
+def feed_builder(create_origin_feed_directory):
+    yield FeedBuilder(create_origin_feed_directory)
 
 
 def build_config(config_path, config_object):
@@ -73,16 +101,17 @@ def check_the_download_directory():
     pass
 
 
-def test_answer(secure_config_file, create_temporary_directory):
-    rss_file = FeedBuilder().add_entry().add_entry().build()
-
+def test_answer(
+    secure_config_file, feed_builder, create_download_destination_directory
+):
+    rss_file = feed_builder.add_entry().add_entry().build()
     build_config(
         secure_config_file,
         {
             "podcasts": [
                 {
                     "name": "test",
-                    "path": create_temporary_directory,
+                    "path": create_download_destination_directory,
                     "rss_link": rss_file,
                 }
             ]
