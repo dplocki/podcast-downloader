97df20a7eb48ec156436f618728de99eb4edd98b
Dawid Płocki <dawid.plocki@gmail.com>
2024-01-27 23:37:16 +0100

    E2E: add the test_configuration_file_name_template_option

--- e2e/test_config_options.py
+++ e2e/test_config_options.py
@@ -81,3 +81,34 @@ def test_configuration_podcast_extensions_option(
 
     # Assert
     podcast_directory.is_containing_only([file_name.lower() for file_name in avi_files])
+
+
+def test_configuration_file_name_template_option(
+    feed: FeedBuilder,
+    use_config: Callable[[Dict], None],
+    podcast_directory: PodcastDirectory,
+):
+    mp3_files_date = [generate_random_mp3_file() for _ in range(generate_random_int())]
+
+    for file_name in mp3_files_date:
+        feed.add_entry(file_name=file_name)
+
+    use_config(
+        {
+            "if_directory_empty": "download_all_from_feed",
+            "podcasts": [
+                {
+                    "file_name_template": "%file_name%_terminus_est_%file_extension%",
+                    "name": generate_random_string(),
+                    "path": podcast_directory.path(),
+                    "rss_link": feed.get_feed_url(),
+                }
+            ],
+        }
+    )
+
+    # Act
+    run_podcast_downloader()
+
+    # Assert
+    podcast_directory.is_containing_only([file_name.lower().replace('.', '_terminus_est_') for file_name in mp3_files_date])
