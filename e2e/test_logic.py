64ea7c64c6b6f6bb9429178514b905c04485a2e3
Dawid Płocki <dawid.plocki@gmail.com>
2024-02-22 22:09:24 +0100

    E2E: Add test test_configuration_during_filling_up_gaps_should_not_download_existing_files

--- e2e/test_logic.py
+++ e2e/test_logic.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from typing import Callable, Dict
 from e2e.fixures import (
     FeedBuilder,
@@ -75,3 +76,49 @@ def test_ignore_files_not_being_part_of_the_feed(
         [file_name.lower() for file_name in not_podcasts_files]
         + [last_podcast_file.lower()]
     )
+
+
+def test_configuration_during_filling_up_gaps_should_not_download_existing_files(
+    feed: FeedBuilder,
+    use_config: Callable[[Dict], None],
+    podcast_directory: PodcastDirectory,
+):
+    # Arrange
+    files_downloaded_and_removed = call_n_times(generate_random_mp3_file)
+    downloaded_files_before_gap = call_n_times(generate_random_mp3_file)
+    files_in_the_gap = call_n_times(generate_random_mp3_file)
+    downloaded_files_after_gap = call_n_times(generate_random_mp3_file)
+    files_to_download = call_n_times(generate_random_mp3_file)
+
+    for file_name in chain(
+        files_downloaded_and_removed,
+        downloaded_files_before_gap,
+        files_in_the_gap,
+        downloaded_files_after_gap,
+        files_to_download,
+    ):
+        feed.add_entry(file_name)
+
+    for file_name in chain(
+        downloaded_files_before_gap,
+        downloaded_files_after_gap,
+    ):
+        podcast_directory.add_file(file_name)
+
+    use_config(
+        {
+            "podcasts": [
+                {
+                    "path": podcast_directory.path(),
+                    "rss_link": feed.get_feed_url(),
+                    "fill_up_gaps": True,
+                }
+            ],
+        }
+    )
+
+    # Act
+    run_podcast_downloader()
+
+    # Assert
+    assert set(feed.get_requested_files_list()) == set(files_to_download)
